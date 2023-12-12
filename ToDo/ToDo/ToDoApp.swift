ad1cfdca3832be28b6f4e3c5c05c9b1b651fad00
Sobaan Azam <129197176+sobaanazam@users.noreply.github.com>
2023-12-12 16:06:01 -0600

    Add files via upload

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Sobaan Azam on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
